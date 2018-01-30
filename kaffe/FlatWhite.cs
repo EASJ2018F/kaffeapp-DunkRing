1c79805d79dd5dcc6c7aeac3e2f580c8ae43f4ef
DunkRing <Chris31@live.dk>
2018-01-30 13:24:41 +0100

    Flatwhite
    Implementeret

--- kaffe/FlatWhite.cs
+++ kaffe/FlatWhite.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace kaffe
 {
-    public class Latte : Kaffe , Imælk
+    class FlatWhite : Kaffe , Imælk
     {
-        /// <summary>
-        /// Hvor meget mælk skal bruges til Latte
-        /// </summary>
-        /// <returns></returns>
+
+        public FlatWhite()
+        { }
+
         public int MlMælk()
         {
-            return 120;
+            return 160;
         }
-        
 
         public override int Pris()
         {
-            return 40;
+            return 45;
         }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
     }
 }
